c9fb049e85aac952eca4b5b05a67d06bac38833f
Tigradcom <tigradcom2@gmail.com@>
2022-06-26 22:33:11 +0400

    Reformat code

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,13 +1,12 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
 public class BonusServiceTest {
 
     @ParameterizedTest
     @CsvFileSource(resources = "/data.csv")
-    public void shouldCalculateForRegisteredAndUnderLimit(int expected, int amount, boolean registred) {
+    public void shouldCalculate(int expected, int amount, boolean registred) {
         BonusService service = new BonusService();
         long actual = service.calculate(amount, registred);
         Assertions.assertEquals(expected, actual);
